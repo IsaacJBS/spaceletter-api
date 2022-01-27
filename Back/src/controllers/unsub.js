39853cfd313f522570d5a1ba1ced0ddb01de51bc
Isaac <isaac.jordaobs@gmail.com>
2022-01-27 15:25:14 -0300

    fix: consertando a query de deletar email

--- Back/src/controllers/unsub.js
+++ Back/src/controllers/unsub.js
@@ -5,23 +5,23 @@ const unsub = async (req, res) => {
     const { email } = req.body;
 
     try {
-        await validationEmail.validate();
+        await validationEmail.validate(req.body);
 
         const querySearchEmail = 'SELECT * FROM emails WHERE email = $1';
         const searchingEmail = await connection.query(querySearchEmail, [email])
 
         if (searchingEmail.rowCount === 0) {
-            return res.status(404).json(`Não foi encontrado o email: ${email} no banco de dados`);
+            return res.status(404).json(`Não foi encontrado o email: ${email} no banco de dados`)
         }
 
-        const queryDeleteEmail = 'DELETE * FROM emails WHERE email = $1';
+        const queryDeleteEmail = 'DELETE FROM emails WHERE email = $1';
         const deletingEmail = await connection.query(queryDeleteEmail, [email]);
 
         if (deletingEmail.rowCount === 0) {
             return res.status(404).json('Não foi possível excluir o e-mail');
         }
 
-        return res.status(204).json();
+        return res.status(204).json({ mensagem: 'E-mail descadastrado com sucesso' });
     } catch (error) {
         res.status(404).json(error.message)
     }
