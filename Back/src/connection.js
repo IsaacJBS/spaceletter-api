e3a718737c3922ebd605329bd65a484a5b9e00ef
Isaac <isaac.jordaobs@gmail.com>
2022-02-21 20:21:37 -0300

    fix: consertando conexao do pg no heroku

--- Back/src/connection.js
+++ Back/src/connection.js
@@ -7,7 +7,10 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const query = (text, param) => {
