dc947410257bb97321d60e4b64cf9eff994ad08e
Isaac <isaac.jordaobs@gmail.com>
2022-02-21 20:16:02 -0300

    adicionando database-url

--- Back/src/connection.js
+++ Back/src/connection.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
