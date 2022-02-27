cfbfac4cfde1662d622872621fde702dc8c72c36
Isaac <isaac.jordaobs@gmail.com>
2022-02-27 19:43:38 -0300

    fix: adicionando swagger para documentar a api

--- Back/src/server.js
+++ Back/src/server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../swagger.json')));
+
 app.use(routes);
 
-module.exports = app
+
+module.exports = app;
